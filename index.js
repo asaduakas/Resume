ca5a18185b948ed8ffbf98eb5d08a41c6a98a33e
Arman Saduakas <asaduakas@wpi.edu>
2023-08-11 18:30:23 -0400

    basic layout and a couple of sections are done

---
+++ index.js
@@ -0,0 +1,94 @@
+document.addEventListener("DOMContentLoaded", function () {
+  animateProgressBarJava(0, 100, 1500); // Start at 0, end at 100, animation duration 1000ms
+  animateProgressBarJs(0, 80, 1500);
+  animateProgressBarPython(0, 70, 1500);
+  animateProgressBarSQL(0,80,1500);
+  animateProgressBarGit(0,100,1500);
+});
+
+let windowWasScrolled = false;
+
+document.addEventListener('wheel', wheelListener, {passive: false}); 
+function wheelListener(e){
+    if(!windowWasScrolled){
+        e.preventDefault();
+        windowWasScrolled = true;
+        var skills = document.getElementById('skills');
+        skills.scrollIntoView({behavior: "smooth", block: "center"});
+    }
+}
+
+function animateProgressBarJava(startValue, endValue, duration) {
+  var progressBar = document.getElementById('java');
+  var increment = (endValue - startValue) / (duration / 10); // Increment for each step
+
+  var interval = setInterval(function () {
+    startValue += increment;
+    progressBar.style.width = startValue + "%";
+    progressBar.setAttribute("aria-valuenow", startValue);
+
+    if (startValue >= endValue) {
+      clearInterval(interval);
+    }
+  }, 10);
+}
+
+function animateProgressBarJs(startValue, endValue, duration) {
+    var progressBar = document.getElementById('js');
+    var increment = (endValue - startValue) / (duration / 10); // Increment for each step
+    
+    var interval = setInterval(function() {
+      startValue += increment;
+      progressBar.style.width = startValue + '%';
+      progressBar.setAttribute('aria-valuenow', startValue);
+      
+      if (startValue >= endValue) {
+        clearInterval(interval);
+      }
+    }, 10);
+}
+
+function animateProgressBarPython(startValue, endValue, duration) {
+    var progressBar = document.getElementById('py');
+    var increment = (endValue - startValue) / (duration / 10); // Increment for each step
+    
+    var interval = setInterval(function() {
+      startValue += increment;
+      progressBar.style.width = startValue + '%';
+      progressBar.setAttribute('aria-valuenow', startValue);
+      
+      if (startValue >= endValue) {
+        clearInterval(interval);
+      }
+    }, 10);
+  }
+
+  function animateProgressBarSQL(startValue, endValue, duration) {
+    var progressBar = document.getElementById('sql');
+    var increment = (endValue - startValue) / (duration / 10); // Increment for each step
+    
+    var interval = setInterval(function() {
+      startValue += increment;
+      progressBar.style.width = startValue + '%';
+      progressBar.setAttribute('aria-valuenow', startValue);
+      
+      if (startValue >= endValue) {
+        clearInterval(interval);
+      }
+    }, 10);
+  }
+
+  function animateProgressBarGit(startValue, endValue, duration) {
+    var progressBar = document.getElementById('git');
+    var increment = (endValue - startValue) / (duration / 10); // Increment for each step
+    
+    var interval = setInterval(function() {
+      startValue += increment;
+      progressBar.style.width = startValue + '%';
+      progressBar.setAttribute('aria-valuenow', startValue);
+      
+      if (startValue >= endValue) {
+        clearInterval(interval);
+      }
+    }, 10);
+  }
